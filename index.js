bd327f9c6391eec8fcdca5c7e2ba5f5b2cd66f42
Sebastian <sebastian.robalino@inpsercom.com>
2023-01-27 09:15:49 -0500

    new: del log of service

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function start(client) {
     client.onMessage((message) => {
         if (message.body === 'Hi') {
             client.startTyping(message.from);
-            console.log(message);
             setTimeout(() => {
                 client.stopTyping(message.from);
                 client.sendText(message.from, 'Hola como stas');
